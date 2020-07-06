929595c43ee1331bd65237a968222044d8d2f4a2
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-07-06 19:05:24 +0530

    Create Nav.jsx

---
+++ src/task-tracker/Nav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Nav = ({ showCardMenu }) => {
+  return (
+    <div className="nav-header">
+      <button onClick={showCardMenu}>Create Day</button>
+    </div>
+  );
+};
+export default Nav;
